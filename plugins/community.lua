5b65a3239413c02c8b2445f2c27692477e79eaa0
engelphi <philipp.engel.1990@googlemail.com>
2023-07-13 14:21:33 +0200

    add community package and clangd config

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,15 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+    { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
+    { import = "astrocommunity.pack.rust" },
+    { import = "astrocommunity.pack.cpp" },
+    { import = "astrocommunity.pack.cmake" },
+    { import = "astrocommunity.pack.python" },
+    { import = "astrocommunity.pack.lua" },
+    { import = "astrocommunity.pack.bash" },
+    { import = "astrocommunity.pack.json" },
+    { import = "astrocommunity.pack.yaml" },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
